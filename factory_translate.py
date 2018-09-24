483fd537ae8dc52406869cf9b08315e8c51b7571
Giulia Naponiello <gnaponie@redhat.com>
2018-09-25 01:01:26 +0200

    Database and first Hello World

---
+++ factory_translate.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ models.py
@@ -0,0 +1,32 @@
+from factory_translate import db
+
+
+class Text(db.Model):
+    __tablename__ = 'text'
+
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text, nullable=False)
+    working_on = db.Column(db.Integer)
+
+    def __init__(self, text, working_on):
+        self.text = text
+        self.working_on = working_on
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+
+class Translation(db.Model):
+    __tablename__ = 'translation'
+
+    id = db.Column(db.Integer, primary_key=True)
+    translated_text = db.Column(db.Text)
+    notes = db.Column(db.Text)
+    text_id = db.Column(db.Integer, db.ForeignKey('text.id'))
+
+    def __init__(self, translated_text, notes):
+        self.translated_text = translated_text
+        self.notes = notes
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
